25abc9b0a3e19629470c38d7271a183473fc4b7f
Christ Emmanuella <christemmanuella03@gmail.com>
2025-03-21 17:42:19 +0100

    upgrade funcs

--- wv_funcs.sql
+++ wv_funcs.sql
@@ -1,51 +1,51 @@
-Delimiter$$
-create function Random position(nrows_INT, ncol_INT)returns VACHAR(20)
-begin
-     declare pos_x int;
-     declare pos_y int;
-     declare position vachar(20);
-
-
-     set pos_x=floor(rand()*nb_lignes) + 1;
-      set pos_x=floor(rand()*colonnes) + 1;
-     set position=concat(pos_x,',', pos_y);
-     return position;
-
-    end $$;
-
-create function random_role()
-returns varchar(10)
-begin
-    declare role varchar(10);
-    
-    if (select count(*) from joueurs where role = 'loup') < (select total_loups from parametres) then
-        set role = 'loup';
-    else
-        set role = 'villageois';
-    end if;
-
-    return role;
-end $$
-
-create function get_the_winner(partyid int)
+create function random_position(@partyid int)
 returns table
-begin
-    return ( 
-        select 
-            j.nom as nom_du_joueur,
-            j.role,
-            p.nom as nom_de_la_partie,
-            (select count(*) from tours where joueur_id = j.id) as nb_tours_joues,
-            (select count(*) from tours where partie_id = partyid) as nb_total_tours,
-            avg(t.temps_decision) as temps_moyen_decision
-        from joueurs j
-        join parties p on j.partie_id = p.id
-        join tours t on j.id = t.joueur_id
-        where j.partie_id = partyid
-        group by j.id
-        order by nb_tours_joues desc
-        limit 1
-    );
-end $$
+as
+return (
+    select top 1 
+        char(65 + abs(checksum(newid())) % 10) as col,
+        cast(1 + abs(checksum(newid())) % 10 as varchar) as row
+    from players_play p
+    where not exists (
+        select 1 from players_play where id_party = @partyid 
+        and origin_position_col = col and origin_position_row = row
+    )
+);
 
-delimiter ;
+go
+
+create function random_role(@partyid int)
+returns varchar(10)
+as
+begin
+    declare @nb_players int, @nb_wolves int, @nb_villagers int, @role varchar(10)
+    select @nb_players = count(*) from players_in_parties where id_party = @partyid
+    select @nb_wolves = (@nb_players * 20) / 100, @nb_villagers = @nb_players - @nb_wolves
+    if (select count(*) from players_in_parties where id_party = @partyid and id_role = 1) < @nb_wolves
+        set @role = 'loup'
+    else
+        set @role = 'villageois'
+    return @role
+end
+
+go
+
+create function get_the_winner(@partyid int)
+returns table
+as
+return (
+    select top 1 p.pseudo, r.description_role, pa.title_party,
+        count(distinct pl.id_turn) as nb_tours_joues,
+        (select count(distinct id_turn) from turns where id_party = @partyid) as nb_tours_total,
+        avg(datediff(second, pl.start_time, pl.end_time)) as temps_moyen_decision
+    from players p
+    join players_in_parties pip on p.id_player = pip.id_player
+    join roles r on pip.id_role = r.id_role
+    join parties pa on pip.id_party = pa.id_party
+    join players_play pl on p.id_player = pl.id_player
+    where pip.id_party = @partyid and pip.is_alive = 'yes'
+    group by p.pseudo, r.description_role, pa.title_party
+    order by nb_tours_joues desc
+);
+
+go
