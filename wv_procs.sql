b6cd1671c7878b60f78b25c56149882ab07c38df
christina <christina.gapy@gmail.com>
2025-03-21 16:37:54 +0100

    add procedure

--- wv_procs.sql
+++ wv_procs.sql
@@ -7,17 +7,17 @@ BEGIN
 
     WHILE @i <= @NB_PLAYERS
     BEGIN
-        -- Générer un start_time aléatoire
+
         SET @start_time = DATEADD(MINUTE, (RAND() * 1000), GETDATE());
 
-        -- Générer un end_time à partir du start_time avec une durée supplémentaire (ex: 5 à 10 minutes)
-        SET @end_time = DATEADD(MINUTE, (RAND() * 5) + 5, @start_time);  -- 5 à 10 minutes plus tard
 
-        -- Insérer un nouveau tour de jeu dans la table turns
+        SET @end_time = DATEADD(MINUTE, (RAND() * 5) + 5, @start_time); 
+
+
         INSERT INTO turns (id_party, start_time, end_time)
         VALUES (@PARTY_ID, @start_time, @end_time);
 
-        -- Passer à l'itération suivante
+
         SET @i = @i + 1;
     END
 END;
