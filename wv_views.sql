024e32cc366ef4d1d6a194aeae13d1a3603690a7
Christemmanuella <christemmanuella03@gmail.com>
2025-03-28 14:47:18 +0100

    changes to wv_views.sql

--- wv_views.sql
+++ wv_views.sql
@@ -1,40 +1,110 @@
-CREATE VIEW ALL_PLAYERS AS
-SELECT 
-    joueur.nom AS nom_du_joueur,
-    COUNT(DISTINCT partie.id) AS nombre_de_parties_jouees,
-    COUNT(tour.id) AS nombre_de_tours_joues,
-    MIN(tour.date_heure_debut) AS date_premiere_participation,
-    MAX(tour.date_heure_fin) AS date_derniere_action
-FROM 
-    joueur
-JOIN 
-    participation ON joueur.id = participation.joueur_id
-JOIN 
-    partie ON participation.partie_id = partie.id
-JOIN 
-    tour ON tour.partie_id = partie.id
-GROUP BY 
-    joueur.nom
-ORDER BY 
-    nombre_de_parties_jouees DESC,
-    date_premiere_participation ASC,
-    date_derniere_action DESC,
-    nom_du_joueur ASC;
-CREATE VIEW ALL_PLAYERS_ELAPSED_GAME AS
-SELECT 
-    joueur.nom AS nom_du_joueur,
-    partie.nom AS nom_de_la_partie,
-    COUNT(participation.id) AS nombre_de_participants,
-    MIN(tour.date_heure_debut) AS date_premiere_action,
-    MAX(tour.date_heure_fin) AS date_derniere_action,
-    DATEDIFF(SECOND, MIN(tour.date_heure_debut), MAX(tour.date_heure_fin)) AS temps_ecoule_en_secondes
-FROM 
-    joueur
-JOIN 
-    participation ON joueur.id = participation.joueur_id
-JOIN 
-    partie ON participation.partie_id = partie.id
-JOIN 
-    tour ON tour.partie_id = partie.id
-GROUP BY 
-    joueur.nom, partie.nom;
+use werewolfvillage;
+go
+
+-- supprimer les vues existantes pour éviter les conflits
+if exists (select * from sys.views where name = 'all_players')
+    drop view all_players;
+go
+
+if exists (select * from sys.views where name = 'all_players_elapsed_game')
+    drop view all_players_elapsed_game;
+go
+
+if exists (select * from sys.views where name = 'all_players_elapsed_tour')
+    drop view all_players_elapsed_tour;
+go
+
+if exists (select * from sys.views where name = 'all_players_stats')
+    drop view all_players_stats;
+go
+
+-- vue 1 : all_players
+create view all_players as
+select 
+    pl.pseudo,
+    count(distinct pip.id_party) as number_of_parties,
+    count(pp.id_turn) as number_of_turns,
+    min(t.start_time) as first_participation,
+    max(pp.end_time) as last_action
+from dbo.players pl
+left join dbo.players_in_parties pip on pl.id_player = pip.id_player
+left join dbo.players_play pp on pl.id_player = pp.id_player
+left join dbo.turns t on pp.id_turn = t.id_turn
+group by pl.pseudo
+having count(distinct pip.id_party) > 0;
+go
+
+-- vue 2 : all_players_elapsed_game
+create view all_players_elapsed_game as
+select 
+    pl.pseudo,
+    p.title_party,
+    count(distinct pip2.id_player) as number_of_participants,
+    min(pp.start_time) as first_action,
+    max(pp.end_time) as last_action,
+    datediff(second, min(pp.start_time), max(pp.end_time)) as seconds_in_game
+from dbo.players pl
+join dbo.players_in_parties pip on pl.id_player = pip.id_player
+join dbo.parties p on pip.id_party = p.id_party
+join dbo.players_play pp on pl.id_player = pp.id_player
+join dbo.turns t on pp.id_turn = t.id_turn and t.id_party = p.id_party
+join dbo.players_in_parties pip2 on p.id_party = pip2.id_party
+group by pl.pseudo, p.title_party;
+go
+
+-- vue 3 : all_players_elapsed_tour
+create view all_players_elapsed_tour as
+select 
+    pl.pseudo,
+    p.title_party,
+    t.id_turn as tour_number,
+    t.start_time as tour_start_time,
+    pp.end_time as decision_time,
+    datediff(second, t.start_time, pp.end_time) as seconds_in_tour
+from dbo.players pl
+join dbo.players_play pp on pl.id_player = pp.id_player
+join dbo.turns t on pp.id_turn = t.id_turn
+join dbo.parties p on t.id_party = p.id_party;
+go
+
+-- vue 4 : all_players_stats
+create view all_players_stats as
+select 
+    pl.pseudo,
+    r.description_role as role,
+    p.title_party,
+    p.id_party, -- inclure id_party pour l'utiliser dans les sous-requêtes
+    count(distinct pp.id_turn) as tours_played,
+    gs.total_turns as total_tours,
+    case 
+        when r.description_role = 'loup' and 
+             (select count(*) 
+              from dbo.players_in_parties pip2 
+              join dbo.roles r2 on pip2.id_role = r2.id_role 
+              where pip2.id_party = p.id_party 
+              and r2.description_role = 'villageois' 
+              and pip2.is_alive = 'oui') = 0 
+        then 'vainqueur'
+        when r.description_role = 'villageois' and 
+             (select count(*) 
+              from dbo.turns t 
+              where t.id_party = p.id_party) >= gs.total_turns and 
+             (select count(*) 
+              from dbo.players_in_parties pip2 
+              join dbo.roles r2 on pip2.id_role = r2.id_role 
+              where pip2.id_party = p.id_party 
+              and r2.description_role = 'villageois' 
+              and pip2.is_alive = 'oui') > 0 
+        then 'vainqueur'
+        else 'perdant'
+    end as winner,
+    avg(datediff(second, t.start_time, pp.end_time)) as avg_decision_time
+from dbo.players pl
+join dbo.players_in_parties pip on pl.id_player = pip.id_player
+join dbo.parties p on pip.id_party = p.id_party
+join dbo.roles r on pip.id_role = r.id_role
+join dbo.game_settings gs on p.id_party = gs.id_party
+left join dbo.players_play pp on pl.id_player = pp.id_player
+left join dbo.turns t on pp.id_turn = t.id_turn and t.id_party = p.id_party
+group by pl.pseudo, r.description_role, p.title_party, p.id_party, gs.total_turns;
+go
